d6b7b50e309a6241b693a2a084fec7d0024637c0
Henry Hwangbo <henry74@gmail.com>
2019-04-26 21:06:33 -0500

    Clean-up

--- src/data/services/sheets.ts
+++ src/data/services/sheets.ts
@@ -165,20 +165,20 @@ function effectiveFormat(value) {
  * @param {[type]} serial a date value in "Excel Date Serial" format
  */
 function ExcelDateToJSDate(serial) {
-  var utc_days = Math.floor(serial - 25569);
-  var utc_value = utc_days * 86400;
-  var date_info = new Date(utc_value * 1000);
+  const utc_days = Math.floor(serial - 25569);
+  const utc_value = utc_days * 86400;
+  const date_info = new Date(utc_value * 1000);
 
-  var fractional_day = serial - Math.floor(serial) + 0.0000001;
+  const fractional_day = serial - Math.floor(serial) + 0.0000001;
 
-  var total_seconds = Math.floor(86400 * fractional_day);
+  let total_seconds = Math.floor(86400 * fractional_day);
 
-  var seconds = total_seconds % 60;
+  const seconds = total_seconds % 60;
 
   total_seconds -= seconds;
 
-  var hours = Math.floor(total_seconds / (60 * 60));
-  var minutes = Math.floor(total_seconds / 60) % 60;
+  const hours = Math.floor(total_seconds / (60 * 60));
+  const minutes = Math.floor(total_seconds / 60) % 60;
 
   return new Date(
     date_info.getFullYear(),
